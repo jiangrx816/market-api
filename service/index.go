3fa6a53671185012559889905d36357b6697bb04
若熙~ <oncwnuE9cvc3j9g4Yk0AFSNyIEF0@git.weixin.qq.com>
2024-03-17 12:31:18 +0800

    修改

--- service/index.go
+++ service/index.go
@@ -26,9 +26,11 @@ func (ins *IndexService) ApiGetTagList() (tagList []model.ZMTags) {
 }
 
 type FormatData struct {
-	Value   string `json:"value"`
-	Name    string `json:"name"`
-	Checked bool   `json:"checked"`
+	Value     string `json:"value"`
+	Name      string `json:"name"`
+	Checked   bool   `json:"checked"`
+	Number    int    `json:"number"`
+	NumberExt int    `json:"number_ext"`
 }
 
 //ApiGetPayList 获取会员价格的列表信息
@@ -44,6 +46,8 @@ func (ins *IndexService) ApiGetPayList() (payListData []FormatData) {
 		if payList[idx].Id == 1 {
 			temp.Checked = true
 		}
+		temp.Number = payList[idx].Number
+		temp.NumberExt = payList[idx].NumberExt
 		temp.Value = strconv.Itoa(payList[idx].Id)
 		temp.Name = payList[idx].Name + "（￥" + payList[idx].CPrice + "）"
 		payListData = append(payListData, temp)
